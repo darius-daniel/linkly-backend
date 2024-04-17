ed4d6b764ddeac8af63ffbfe94329176ae7e708b
Darius Daniel <dariusdaniel771@gmail.com>
2024-04-17 15:13:57 +0100

    Created a new production branch

--- src/db/db_connection.js
+++ src/db/db_connection.js
@@ -1,14 +1,17 @@
 import { Sequelize, DataTypes } from 'sequelize';
 
-const sequelize = new Sequelize('Linkly', 'root', 'root', {
-  host: 'localhost',
-  dialect: 'mariadb',
+const sequelize = new Sequelize('Linkly', 'darius', 'D9G8Va9PY7MSVL6Li%40iu', {
+  host: 'nnwplcuask.sqlite.cloud:8860',
+  dialect: 'sqlite',
+  logging: false,
 });
 
 async function testConnection() {
   try {
     await sequelize.authenticate();
-    console.log('Connection to the database has been established successfully.');
+    console.log(
+      'Connection to the database has been established successfully.'
+    );
   } catch (error) {
     console.error('Unable to connect to the database:', error.message);
   }
