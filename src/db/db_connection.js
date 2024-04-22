c0cc736fd347c379b8a5ded78ebdd0dab7721471
Darius Daniel <dariusdaniel771@gmail.com>
2024-04-22 15:43:35 +0100

    Changed database to vercel

--- src/db/db_connection.js
+++ src/db/db_connection.js
@@ -1,14 +1,23 @@
-import { Sequelize, DataTypes } from 'sequelize';
+import { Sequelize } from 'sequelize';
+import { PostgresDialect } from '@sequelize/postgres';
 
-const sequelize = new Sequelize('Linkly', 'root', 'root', {
-  host: 'localhost',
-  dialect: 'mariadb',
+const sequelize = new Sequelize({
+  dialect: PostgresDialect,
+  database: 'linkly_db',
+  user: 'default',
+  password: 'irSv2BA9DjUg',
+  host: 'ep-lucky-paper-a41r2r8q.us-east-1.aws.neon.tech',
+  port: 5432,
+  ssl: true,
+  clientMinMessages: 'notice',
 });
 
 async function testConnection() {
   try {
     await sequelize.authenticate();
-    console.log('Connection to the database has been established successfully.');
+    console.log(
+      'Connection to the database has been established successfully.'
+    );
   } catch (error) {
     console.error('Unable to connect to the database:', error.message);
   }
